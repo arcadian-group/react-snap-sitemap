1226817fa49b51713c785b7e4bd5b8e352da8562
Seagyn Davis <seagyn.davis@leadhome.co.za>
2019-10-17 08:14:40 +0200

    fix: 🐛 no trailing forward slash

--- run.js
+++ run.js
@@ -75,7 +75,7 @@ const readSite = async (dir) => {
 (async () => {
     await readSite('build');
 
-    const siteUrls = allRoots.map(root => root.replace('build/', baseUrl).replace('/index.html', '/'));
+    const siteUrls = allRoots.map(root => root.replace('build/', baseUrl).replace('/index.html', ''));
 
     const urlset = builder.create('urlset', { encoding: 'UTF-8', version: '1.0' });
 
